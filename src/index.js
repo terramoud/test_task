4d45214c44dce90af7dd14fbcf7eebf3f1404623
Alexander <terramoud@gmail.com>
2020-03-24 21:47:10 +0200

    fixed nav menu for mobile version

--- src/index.js
+++ src/index.js
@@ -3,12 +3,7 @@ require('./index.html');
 require('./jquery-3.4.1.min');
 require('./index.scss');
 
-console.log(jQuery('#menu'));
 
-
-function ghostMenuNav() {
-
-}
 
 jQuery('#ghost-menu__nav').mouseenter(function (event) {
   jQuery(this).children('.nav-menu').addClass('nav-menu_active');
@@ -16,7 +11,34 @@ jQuery('#ghost-menu__nav').mouseenter(function (event) {
 
 jQuery('#ghost-menu__nav').children('.nav-menu').mouseleave(function (event) {
   jQuery(this).removeClass('nav-menu_active');
+
+  jQuery(this).find('.menu__extra').removeClass('menu__extra_theme_visible');
+  jQuery(this).find('.menu__link').removeClass('menu__link_theme_grey');
+  jQuery(this).find('.extra-menu').removeClass('extra-menu_theme_grey');
 });
+
+jQuery('#menu').find('.menu__link').each(function (event) {
+  this.addEventListener("touchend", handleEnd, true);
+});
+
+function handleEnd(e) {
+
+  if (jQuery(e.target).hasClass('menu__link_theme_grey')){
+    e.target.removeEventListener("touchend", handleEnd, true);
+    console.log(e.target);
+  } else {
+    e.preventDefault();
+  }
+
+  jQuery('.menu__extra').removeClass('menu__extra_theme_visible');
+  jQuery('.menu__link').removeClass('menu__link_theme_grey');
+  jQuery('.extra-menu').removeClass('extra-menu_theme_grey');
+
+  jQuery(e.target).addClass('menu__link_theme_grey');
+  jQuery(e.target).next('.menu__extra').addClass('menu__extra_theme_visible');
+  jQuery(e.target).next('.menu__extra').find('.extra-menu').addClass('extra-menu_theme_grey');
+
+}
 
 jQuery('#menu').find('.menu__link').mouseenter(function (event) {
   jQuery(event.target).addClass('menu__link_theme_grey');
